c27087b50b0d2683b94ed1f94ce8f4cea26fc14c
mailbinoy <110921634+mailbinoy@users.noreply.github.com>
2022-12-26 17:03:20 +0530

    Update sshd_config.tf

--- terraform/sshd_config.tf
+++ terraform/sshd_config.tf
@@ -1,4 +1,4 @@
 resource "local_file" "sshd_config" {
-    content  = "Host ${aws_instance.jenkins.private_ip} ${aws_instance.app.private_ip}\n   ProxyJump 52.87.221.164"
+    content  = "Host ${aws_instance.jenkins.private_ip} ${aws_instance.app.private_ip}\n   ProxyJump ${aws_instance.bastion.public_ip}"
     filename = "/home/ubuntu/.ssh/config"
 }
