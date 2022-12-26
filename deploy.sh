6c7611ad38ae2179cdb7f1cdbb478ba73f2eec69
Binoy <mailbinoyv@gmail.com>
2022-12-26 15:03:32 +0530

    fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo Logging to ECR...'
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 223316186016.dkr.ecr.us-east-1.amazonaws.com'
+echo 'Logging to ECR...'
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 223316186016.dkr.ecr.us-east-1.amazonaws.com
 #docker rm -f assignment
-docker run -d -p 80:8081 223316186016.dkr.ecr.us-east-1.amazonaws.com/assignment:latest
+[ "$(docker ps -a | grep assignment)" ] && docker rm -f assignment ; docker run -d -p 80:8081 --name assignment 223316186016.dkr.ecr.us-east-1.amazonaws.com/assignment:latest
