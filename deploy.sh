eb9697aaeaf2084a91ff19b3d3e3c2f82f452b08
Binoy <mailbinoyv@gmail.com>
2022-12-26 05:57:03 +0000

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Logging to ECR..."
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 223316186016.dkr.ecr.us-east-1.amazonaws.com
+echo "Building Project..."
+docker build -t assignment .
+echo "Docker image tag for AWS ECR..."
+docker tag assignment:latest 223316186016.dkr.ecr.us-east-1.amazonaws.com/assignment:latest
+echo "Image Push to ECR..."
+docker push 223316186016.dkr.ecr.us-east-1.amazonaws.com/assignment:latest
+echo "Running Project..."
+docker run -d -p 80:8081 223316186016.dkr.ecr.us-east-1.amazonaws.com/assignment:latest
+exit
